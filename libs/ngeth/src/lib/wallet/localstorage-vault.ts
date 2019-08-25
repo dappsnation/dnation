62eb26e7abad35bada7dda1954a316799da91c5a
pldespaigne <pl.despaigne@gmail.com>
2019-08-25 11:13:10 +0200

    requested changes

--- libs/ngeth/src/lib/wallet/localstorage-vault.ts
+++ libs/ngeth/src/lib/wallet/localstorage-vault.ts
@@ -18,42 +18,33 @@ export class LocalStorageVault extends Vault {
   // tslint:disable-next-line: unified-signatures
   get(address: string): Promise<string>;
   get(key: KeyVault): Promise<string | string[]> {
-    switch(key) {
-      case 'accounts': return JSON.parse(this.storage.getItem('accounts'));
-      case 'default': return JSON.parse(this.storage.getItem('default'));
-      default: return JSON.parse(this.storage.getItem(key));
-    }
+    return JSON.parse(this.storage.getItem(key));
   }
 
   set(key: 'accounts', accounts: string[]): Promise<any>;
   set(key: 'default', address: string): Promise<any>;
   // tslint:disable-next-line: unified-signatures
   set(address: string, json: string): Promise<any>;
-  set(key: string, payload: string | string[]): Promise<any> {
-    return new Promise((res, rej) => {
+  async set(key: string, payload: string | string[]): Promise<any> {
+    if(key === 'accounts' && !Array.isArray(payload)) {
+      throw new Error('When key is "accounts", payload must be a string[]');
+    }
+    if (key !== 'accounts' && Array.isArray(payload)) {
+      throw new Error('When key is "default" or an address, payload must be a string');
+    }
 
-      if(key === 'accounts' && !Array.isArray(payload)) {
-          rej('When key is "accounts", payload must be a string[]');
-      }
-      if (key !== 'accounts' && Array.isArray(payload)) {
-        rej('When key is "default" or an address, payload must be a string');
-      }
-
-      res(this.storage.setItem(key, JSON.stringify(payload)));
-    });
+    this.storage.setItem(key, JSON.stringify(payload));
   }
 
-  delete(key: KeyVault): Promise<any> {
-    return new Promise(res => {
-      res(this.storage.removeItem(key));
-    })
+  async delete(key: KeyVault): Promise<any> {
+    this.storage.removeItem(key);
   }
 
-  clear(): Promise<any> {
-    return new Promise(res => {
-      this.get('accounts').then(accounts =>
-        accounts.forEach(account => this.delete(account))
-      ).then(() => res());
-    });
+  async clear(): Promise<any> {
+    this.get('accounts').then(accounts =>
+      accounts.forEach(account => this.delete(account))
+    );
+    this.delete('default');
+    this.delete('accounts');
   }
 }
