16bc8488a3967a621f9eaefce65b30e14cd9b205
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-08-25 13:07:37 +0200

    Add network management for ngcontract

--- libs/ngeth/src/lib/contract/ng-contract.ts
+++ libs/ngeth/src/lib/contract/ng-contract.ts
@@ -4,6 +4,7 @@ import { Provider } from '@ethersproject/providers';
 import { Signer } from '@ethersproject/abstract-signer';
 import { NETWORK, PROVIDER } from '../tokens';
 import { Observable, fromEvent } from 'rxjs';
+import { MetaMaskProvider } from '../provider/metamask';
 
 interface IContract {
   methods?: {
@@ -29,15 +30,21 @@ export class NgContract<T extends IContract = any> extends Contract {
     abi: ContractInterface,
     provider?: Provider | Signer
   ) {
-    // Get address
-    const network = inject<string>(NETWORK);
-    const address = addresses[network];
-    if (!address) {
-      throw new Error(`No address found for contract on network ${network}`);
-    }
     // Get provider
     if (!provider) {
       provider = inject<Provider>(PROVIDER);
+    }
+    // Get network
+    const network = provider instanceof MetaMaskProvider
+      ? provider.networkVersion
+      : inject<string>(NETWORK);
+    if (!network) {
+      throw new Error('No network provided');
+    }
+    // Get address
+    const address = addresses[network];
+    if (!address) {
+      throw new Error(`No address found for contract on network ${network}`);
     }
     super(addresses[network], abi, provider);
   }
